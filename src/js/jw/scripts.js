ad0146f81e64f66e8549b2e4514ab64a780b66c6
unknown <jwojtkielewicz@jwojkielewicz.internal.mywebgrocer.com>
2016-02-24 13:30:43 -0500

    Revised jw scss name, updated src/jw/scripts.js to take different box sizes (for javascript's part, at least). Included jw scss in cs build.

--- src/js/jw/scripts.js
+++ src/js/jw/scripts.js
@@ -10,13 +10,15 @@ function init() {
 	// 2. create the container that will be used to rotate its children, and add it to the stage
 	container = new Sprite3D().setZ(-500).update();
 	stage.addChild( container );
-	
+  
+  var boxDepth = 280;
+  var boxHeight = 1172;
+  var boxWidth = 900;
+  
   container.addChild(
     new Sprite3D()
       .setClassName("boxFront")
-      .setPosition(-450, -586, 0)
-      // .rotateY(0)
-      // .rotateX(0)
+      .setPosition(-1*(boxWidth/2), -1*(boxHeight/2), 0)
       .setRotateFirst(true)
       .update()
   );
@@ -25,7 +27,7 @@ function init() {
     new Sprite3D()
       .setClassName("boxRight")
       .setTransformOrigin( 0, 0 )
-      .setPosition(0, -586, 450)
+      .setPosition(0, -1*(boxHeight/2), (boxWidth/2))
       .rotateY(90)
       .setRotateFirst(true)
       .update()
@@ -35,7 +37,7 @@ function init() {
     new Sprite3D()
       .setClassName("boxLeft")
       .setTransformOrigin( 0, 0 )
-      .setPosition(-280, -586, 450)
+      .setPosition(-280, -1*(boxHeight/2), (boxWidth/2))
       .rotateY(-90)
       .setRotateFirst(true)
       .update()
@@ -45,7 +47,7 @@ function init() {
     new Sprite3D()
       .setClassName("boxBack")
       .setTransformOrigin( 0, 0 )
-      .setPosition(-450, -586, 280)
+      .setPosition(-1*(boxWidth/2), -1*(boxHeight/2), boxDepth)
       .rotateY(-180)
       .setRotateFirst(true)
       .update()
@@ -55,7 +57,7 @@ function init() {
     new Sprite3D()
       .setClassName("boxTop")
       .setTransformOrigin( 0, 0 )
-      .setPosition(-450, 0, 586)
+      .setPosition(-1*(boxWidth/2), 0, (boxHeight/2))
       .rotateX(90)
       .rotateY(180)
       .setRotateFirst(true)
@@ -66,7 +68,7 @@ function init() {
     new Sprite3D()
       .setClassName("boxBottom")
       .setTransformOrigin( 0, 0 )
-      .setPosition(-450, -280, 586)
+      .setPosition(-1*(boxWidth/2), -1*boxDepth, boxHeight/2)
       .rotateX(-90)
       .rotateY(-180)
       .setRotateFirst(true)
@@ -85,6 +87,6 @@ function move() {
 	// rotate the container around the X and Y axis, then apply these transformations
 	container
 		.rotateY( -1 )
-		.setRotationX( Math.cos(t) * 30 + 30 )
+		.setRotationX( Math.cos(t) * 30 - 30 )
 		.update();
 }
