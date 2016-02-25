74aa179357a2ee23d1dbe95fb1c7e3d303d5c9d8
Patrick Mulcahy <pmulcahy@mywebgrocer.com>
2016-02-25 12:34:27 -0500

    updates for product box
    individual products are now interactable. shelf styles are done

--- src/js/productBox.js
+++ src/js/productBox.js
@@ -1,19 +1,99 @@
-var xAngle = -25;
-var xOrigin = xAngle;
-var yAngle = 25;
-var yOrigin = yAngle;
-var scale = 1;
-var scaleOrigin = scale;
+var xAngle = -25,
+    xOrigin = 0;
+
+var yAngle = 25,
+    yOrigin = 0;
+
+var xTranslateOrigin = 0,
+    xTranslate = xTranslateOrigin;
+
+var yTranslateOrigin = 0,
+    yTranslate = yTranslateOrigin;
+
+var scale = 1,
+    scaleOrigin = scale;
+
 var moveRate = 10;
 var snapToOn = false;
 
-function smoothRotate(direction) {
+var $focusedProduct = null;
+
+function getCurrentTransformValues () {
+  return "translateX(" + xTranslate + "%)" +
+          "translateY(" + yTranslate + "%)" +
+          "rotateX(" + xAngle + "deg) " +
+          "rotateY(" + yAngle + "deg) " +
+          "scale3d(" + scale + ", " + scale + ", " + scale + ")";
+}
+
+// uses transalateX/Y to offset focused product based on position on shelf
+//   to help ensure the focused product is fully visible
+function determineFocusPosition () {
+  var productOrder = $focusedProduct.index(),
+      productTotal = $focusedProduct.siblings().length + 1,
+      parentOrder = $focusedProduct.parent().index();
+
+  if (productOrder < 2) {
+    xTranslate = 50;
+  } else if (productOrder - productTotal <= 2) {
+    xTranslate = -50;
+  }
+
+  if (parentOrder == 0) {
+    yTranslate = 50;
+  } else if (parentOrder == 1) {
+    xTranslate = -50;
+  }
+}
+
+// focuses product be scaling it up and positioning it in the view
+function focusProduct ($newTarget) {
+  $focusedProduct = $newTarget;
+  scale = 2.5;
+
+  determineFocusPosition();
+  $focusedProduct.addClass("productBox--focused")
+                  .css({
+                    "z-index": "999",
+                    "-webkit-transform":  getCurrentTransformValues()
+                  });
+}
+
+// resets focused product by clearing its js-modified values
+// nulls $focusedProduct to deactivate keyboard functions
+function resetFocus () {
+  if ($focusedProduct != null) {
+    xAngle = xOrigin;
+    yAngle = yOrigin;
+    scale = scaleOrigin;
+    xTranslate = xTranslateOrigin;
+    yTranslate = yTranslateOrigin;
+
+    $focusedProduct.removeClass("productBox--focused")
+                    .css({
+                      "z-index": "initial",
+                      "-webkit-transform": getCurrentTransformValues()
+                    });
+
+    $focusedProduct = null;
+  }
+}
+
+// manages all keyboard interactions
+function smoothRotate (event, handler) {
+  if (!$focusedProduct) {
+    return;
+  }
+
+  event.preventDefault();
+  var direction = handler.shortcut;
+
   // transition duration is varied between rotation types (1s for snap-to, 10ms for manual)
   if (key.shift && !snapToOn) {
-    $(".productBox--cornpops").css("-webkit-transition", "-webkit-transform 750ms linear");
+    $focusedProduct.css("-webkit-transition", "-webkit-transform 750ms linear");
     snapToOn = true;
   } else if (!key.shift && snapToOn) {
-    $(".productBox--cornpops").css("webkit-transition", "-webkit-transform 500ms linear");
+    $focusedProduct.css("webkit-transition", "-webkit-transform 250ms linear");
     snapToOn = false;
   }
 
@@ -31,6 +111,18 @@ function smoothRotate(direction) {
       break;
     case "right":
       yAngle = yAngle + moveRate;
+      break;
+    case "ctrl+up":
+      yTranslate -= moveRate;
+      break;
+    case "ctrl+down":
+      yTranslate += moveRate;
+      break;
+    case "ctrl+left":
+      xTranslate -= moveRate;
+      break;
+    case "ctrl+right":
+      xTranslate += moveRate;
       break;
     case "shift+up":
       diff = xAngle % 90;
@@ -73,31 +165,32 @@ function smoothRotate(direction) {
     case "shift+enter":
       xAngle = xOrigin;
       yAngle = yOrigin;
-      scale = scaleOrigin;
+      // scale = scaleOrigin;
+      // xTranslate = xTranslateOrigin;
+      // yTranslate = yTranslateOrigin;
       break;
   }
 
-  $(".productBox--cornpops").css("-webkit-transform", "rotateX(" + xAngle + "deg) " +
-                                    "rotateY(" + yAngle + "deg) " +
-                                    "scale3d(" + scale + ", " + scale + ", " + scale + ")");
-
-  // log key event and resulting coordinates
-  console.log(direction + " : x=" + xAngle + " : y=" + yAngle + " : scale=" + scale);
+  $focusedProduct.css("-webkit-transform", getCurrentTransformValues());
 }
 
-// free rotate & zoom
-key('up, down, left, right, ctrl+=, ctrl+-', function(event, handler) {
-    event.preventDefault();
-    smoothRotate(handler.shortcut);
-});
+// keyboard controls for focused product
+key('up, down, left, right, ctrl+=, ctrl+-', smoothRotate);
+key('shift+up, shift+down, shift+left, shift+right', smoothRotate);
+key('ctrl+up, ctrl+down, ctrl+left, ctrl+right', smoothRotate);
+key('shift+enter', smoothRotate);
+key('esc', resetFocus);
 
-// snap-to
-key('shift+up, shift+down, shift+left, shift+right', function(event, handler) {
+$(function () {
+  // target selection logic
+  $('.productBox').on('click', function (event) {
     event.preventDefault();
-    smoothRotate(handler.shortcut);
-});
+    event.stopPropagation();
 
-key('shift+enter', function(event, handler) {
-    event.preventDefault();
-    smoothRotate(handler.shortcut);
+    resetFocus();
+    focusProduct($(this));
+  });
+
+  // clear focused on blur
+  $('body').on('click', resetFocus);
 });
